8f286d5ec1163256985b2fa45ecf96e60d37358d
SeaFalcon <seafalcon323@gmail.com>
2021-05-31 10:55:55 +0900

    Implement an API that receives food data and stores it in the DB
    - Implement route, controller, dao

--- config/express.js
+++ config/express.js
@@ -9,9 +9,11 @@ module.exports = function () {
 
     app.use(compression());
 
-    app.use(express.json());
+    // app.use(express.json());
 
-    app.use(express.urlencoded({ extended: true }));
+    // app.use(express.urlencoded({ extended: true }));
+
+    app.use(express.json({ limit : "50mb" })); app.use(express.urlencoded({ limit:"50mb", extended: false }));
 
     app.use(methodOverride());
 
@@ -24,6 +26,7 @@ module.exports = function () {
     require('../src/app/routes/userRoute')(app);
     require('../src/app/routes/foodRoute')(app);
     require('../src/app/routes/dialysisRoute')(app);
+    require('../src/app/routes/dietRoute')(app);
 
     /* Web */
     // require('../src/web/routes/indexRoute')(app);
