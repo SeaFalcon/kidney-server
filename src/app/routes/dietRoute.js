3e81471b8023efe35e7ef12978c05557009ba9bd
hailly01 <yanghagi@naver.com>
2021-06-04 17:57:53 +0900

    Permute get Diet Route

--- src/app/routes/dietRoute.js
+++ src/app/routes/dietRoute.js
@@ -2,6 +2,6 @@ module.exports = function (app) {
   const diet = require("../controllers/dietController");
   const jwtMiddleware = require("../../../config/jwtMiddleware");
 
-  app.get("/diet-record", jwtMiddleware, diet.getDites);
+  app.get("/diet", jwtMiddleware, diet.getDites);
   app.post("/diet", diet.saveDiet);
 };
