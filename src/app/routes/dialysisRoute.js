292da0cba981c3d752e36921bae5784147a97b1d
haejii <yanghagi@naver.com>
2021-07-14 17:20:36 +0900

    ReDesign dialysisRoute

--- src/app/routes/dialysisRoute.js
+++ src/app/routes/dialysisRoute.js
@@ -1,12 +1,31 @@
-const jwtMiddleware = require('../../../config/jwtMiddleware');
-const dialysis = require('../controllers/dialysisController')
-const { uploadMiddleware } = require('../../../config/s3');
+const jwtMiddleware = require("../../../config/jwtMiddleware");
+const dialysis = require("../controllers/dialysisController");
+const { uploadMiddleware } = require("../../../config/s3");
 module.exports = (app) => {
-  app.post('/hemodialysis-memo', jwtMiddleware, uploadMiddleware.single('image'), dialysis.saveHemodialysisMemo);
-  app.post('/peritoneum-memo', jwtMiddleware, uploadMiddleware.single('image'), dialysis.savePeritonrumMemo);
-  app.get('/hemodialysis-memo', jwtMiddleware, dialysis.getHemodialysisMemo);
-  app.get('/peritoneum-memo', jwtMiddleware, dialysis.getPeritonrumMemo);
-  app.put('/hemodialysis-memo', jwtMiddleware, uploadMiddleware.single('image'), dialysis.changeHemodialysisMemo);
-  app.put('/peritoneum-memo', jwtMiddleware, uploadMiddleware.single('image'), dialysis.changePeritonrumMemo);
-
-}
+  app.post(
+    "/hemodialysis-memo",
+    jwtMiddleware,
+    uploadMiddleware.single("image"),
+    dialysis.saveHemodialysisMemo
+  );
+  app.post(
+    "/peritoneum-memo",
+    jwtMiddleware,
+    uploadMiddleware.single("image"),
+    dialysis.savePeritonrumMemo
+  );
+  app.get("/hemodialysis-memo", jwtMiddleware, dialysis.getHemodialysisMemo);
+  app.get("/peritoneum-memo", jwtMiddleware, dialysis.getWeekPeritonrumMemo);
+  app.put(
+    "/hemodialysis-memo",
+    jwtMiddleware,
+    uploadMiddleware.single("image"),
+    dialysis.changeHemodialysisMemo
+  );
+  app.put(
+    "/peritoneum-memo",
+    jwtMiddleware,
+    uploadMiddleware.single("image"),
+    dialysis.changePeritonrumMemo
+  );
+};
